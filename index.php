eea2db8ba587e423eff106774a7ed901fa177805
Inseok Lee <dlunch@gmail.com>
2012-03-19 22:33:08 +0900

    Add stat, api

--- index.php
+++ index.php
@@ -6,10 +6,13 @@
 	</head>
 
 	<body>
-		URL shortener
-		<form action="short.php">
-			<input type="text" name="destination" />
-			<input type="submit" />
-		</form>
+		<p>
+			URL shortener
+			<form action="short.php">
+				<input type="text" name="destination" />
+				<input type="submit" />
+			</form>
+		</p>
+		Stat : <a href="http://dlun.ch/stat.php">http://dlun.ch/stat.php</a>
 	</body>
 </html>
